0d990be59f547823df12deceeef8e289d492ebeb
avinash <avinash.patel@kuliza.com>
2020-01-13 15:07:08 +0530

    Product Swagger Added

--- FirstPro/src/main/java/com/example/demo/controller/CustomerProductRestController.java
+++ FirstPro/src/main/java/com/example/demo/controller/CustomerProductRestController.java
@@ -36,78 +36,79 @@ import com.example.demo.util.ReflectionUtil;
 @CrossOrigin
 @RequestMapping(path = "/object/cusotmerProduct/")
 public class CustomerProductRestController {
-    @Autowired
-    private CustomerProductService cusService;
+	@Autowired
+	private CustomerProductService cusService;
 
-    ReflectionUtil refUtil = ReflectionUtil.getInstance();
+	ReflectionUtil refUtil = ReflectionUtil.getInstance();
 
-    @PostMapping(path = "create")
-    public ResponseEntity<?> createCusPro(@Valid @RequestBody CustomerProduct customerProduct, BindingResult result) {
-	if (result != null  && result.hasErrors()) {
-	    Map<String, String> error = new HashMap<String, String>();
-	    for (FieldError fieldError : result.getFieldErrors()) {
-		error.put(fieldError.getField(), fieldError.getDefaultMessage());
-	    }
-	    return new ResponseEntity<Map<String, String>>(error, HttpStatus.BAD_REQUEST);
+	@PostMapping(path = "create")
+	public ResponseEntity<?> createCusPro(@Valid @RequestBody CustomerProduct customerProduct, BindingResult result) {
+		if (result != null && result.hasErrors()) {
+			Map<String, String> error = new HashMap<String, String>();
+			for (FieldError fieldError : result.getFieldErrors()) {
+				error.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String, String>>(error, HttpStatus.BAD_REQUEST);
+		}
+		CustomerProduct cp = cusService.createCusotmerProduct(customerProduct);
+		System.out.println(cp);
+		return new ResponseEntity<CustomerProduct>(cp, HttpStatus.CREATED);
 	}
-	CustomerProduct cp = cusService.createCusotmerProduct(customerProduct);
-	return new ResponseEntity<CustomerProduct>(cp, HttpStatus.CREATED);
-    }
 
-    @GetMapping(path = "get/{id}")
-    public ResponseEntity<?> getCustomerProduct(@PathVariable(value = "id") Long id) {
-	CustomerProduct cp = cusService.getCustomerProductById(id);
-	if (cp == null) {
-	    return new ResponseEntity<String>("Nothing Found with id:- " + id, HttpStatus.BAD_REQUEST);
+	@GetMapping(path = "get/{id}")
+	public ResponseEntity<?> getCustomerProduct(@PathVariable(value = "id") Long id) {
+		CustomerProduct cp = cusService.getCustomerProductById(id);
+		if (cp == null) {
+			return new ResponseEntity<String>("Nothing Found with id:- " + id, HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<CustomerProduct>(cp, HttpStatus.OK);
 	}
-	return new ResponseEntity<CustomerProduct>(cp, HttpStatus.OK);
-    }
 
-    @GetMapping(path = "get")
-    public ResponseEntity<?> getAllCusotmerProduct() {
-	List<CustomerProduct> cpList = cusService.getCustomerProductAll();
-	if (cpList.size() == 0) {
-	    return new ResponseEntity<String>("Nothing Found ", HttpStatus.BAD_REQUEST);
+	@GetMapping(path = "get")
+	public ResponseEntity<?> getAllCusotmerProduct() {
+		List<CustomerProduct> cpList = cusService.getCustomerProductAll();
+		if (cpList.size() == 0) {
+			return new ResponseEntity<String>("Nothing Found ", HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<List<CustomerProduct>>(cpList, HttpStatus.OK);
 	}
-	return new ResponseEntity<List<CustomerProduct>>(cpList, HttpStatus.OK);
-    }
 
-    @DeleteMapping(path = "delete/{id}")
-    public ResponseEntity<?> deleteCustomerProductById(@PathVariable(value = "id") Long id) {
-	CustomerProduct cp = cusService.getCustomerProductById(id);
-	if (cp == null) {
-	    return new ResponseEntity<String>("No CustomerProduct found for id:-" + id, HttpStatus.BAD_REQUEST);
+	@DeleteMapping(path = "delete/{id}")
+	public ResponseEntity<?> deleteCustomerProductById(@PathVariable(value = "id") Long id) {
+		CustomerProduct cp = cusService.getCustomerProductById(id);
+		if (cp == null) {
+			return new ResponseEntity<String>("No CustomerProduct found for id:-" + id, HttpStatus.BAD_REQUEST);
+		}
+		cusService.deleteCustoProduct(id);
+		return new ResponseEntity<String>("SuccessFully Deleted The CustomerProduct", HttpStatus.OK);
+
 	}
-	cusService.deleteCustoProduct(id);
-	return new ResponseEntity<String>("SuccessFully Deleted The CustomerProduct", HttpStatus.OK);
 
-    }
+	@PatchMapping(path = "update/{id}")
+	public ResponseEntity<?> updateCustomerProduct(@Valid @RequestBody String cp, @PathVariable(value = "id") Long id)
+			throws ParseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		CustomerProduct cpFromDB = cusService.getCustomerProductById(id);
+		boolean deliveryFlag = cp.contains("productDeliveryDate");
+		if (cpFromDB == null) {
+			return new ResponseEntity<String>("No CustomerProduct with id:- " + id + "Found", HttpStatus.BAD_REQUEST);
+		}
+		JSONParser parser = new JSONParser();
+		JSONObject obj = (JSONObject) parser.parse(cp);
 
-    @PatchMapping(path = "update/{id}")
-    public ResponseEntity<?> updateCustomerProduct(@Valid @RequestBody String cp, @PathVariable(value = "id") Long id)
-	    throws ParseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-	CustomerProduct cpFromDB = cusService.getCustomerProductById(id);
-	boolean deliveryFlag = cp.contains("productDeliveryDate");
-	if (cpFromDB == null) {
-	    return new ResponseEntity<String>("No CustomerProduct with id:- " + id + "Found", HttpStatus.BAD_REQUEST);
+		for (Iterator iterator = ((Map<String, String>) obj).keySet().iterator(); iterator.hasNext();) {
+			String props = (String) iterator.next();
+			if (!props.equals("productDeliveryDate")) {
+				refUtil.getSetterMethod("CustomerProduct", props).invoke(cpFromDB, obj.get(props));
+			}
+		}
+		if (deliveryFlag == true) {
+			String str = (String) obj.get("productDeliveryDate");
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+			LocalDate dateTime = LocalDate.parse(str, formatter);
+
+			cpFromDB.setProductDeliveryDate(dateTime);
+		}
+		cusService.createCusotmerProduct(cpFromDB);
+		return new ResponseEntity<CustomerProduct>(cpFromDB, HttpStatus.OK);
 	}
-	JSONParser parser = new JSONParser();
-	JSONObject obj = (JSONObject) parser.parse(cp);
-
-	for (Iterator iterator = ((Map<String, String>) obj).keySet().iterator(); iterator.hasNext();) {
-	    String props = (String) iterator.next();
-	    if (!props.equals("productDeliveryDate")) {
-		refUtil.getSetterMethod("CustomerProduct", props).invoke(cpFromDB, obj.get(props));
-	    }
-	}
-	if (deliveryFlag == true) {
-	    String str = (String) obj.get("productDeliveryDate");
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-	    LocalDate dateTime = LocalDate.parse(str, formatter);
-
-	    cpFromDB.setProductDeliveryDate(dateTime);
-	}
-	cusService.createCusotmerProduct(cpFromDB);
-	return new ResponseEntity<CustomerProduct>(cpFromDB, HttpStatus.OK);
-    }
 }
